77fef2aad6b6efd06736a2027bc203f86de44418
josebiancoprogeo <jose.bianco@progeorn.com.br>
2025-08-06 11:52:12 -0300

    first

---
+++ windirstat_s3/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace windirstat_s3
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
